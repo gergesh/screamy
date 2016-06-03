55c6d9b2202578d32119edae166af4217d6f098a
ben_kaufman <ben.kaufman10@gmail.com>
2016-06-03 23:35:36 +0300

    Extract scream to a service

--- app/src/main/java/com/screamy/MainActivity.java
+++ app/src/main/java/com/screamy/MainActivity.java
@@ -1,6 +1,7 @@
 package com.screamy;
 
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -13,7 +14,7 @@ import android.hardware.SensorEvent;
 import android.util.Log;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity {
     private SensorManager senSensorManager;
     private Sensor senAccelerometer;
 
@@ -24,55 +25,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        Sensor mySensor = event.sensor;
-
-        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            float x = event.values[0];
-            float y = event.values[1];
-            float z = event.values[2];
-
-            long curTime = System.currentTimeMillis();
-
-            if ((curTime - lastUpdate) > 100) {
-                long diffTime = (curTime - lastUpdate);
-                lastUpdate = curTime;
-
-                float speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
-
-                if (y <= 1.5 && y >= -1.5) {
-                    if (x <= 1.5 && x >= -1.5) {
-                        if (z <= 1.5 && z >= -1.5) {
-                            MediaPlayer mp = MediaPlayer.create(this, R.raw.screamwilhm);
-                            mp.start();
-                        }
-                    }
-                    }
-                }
-                last_x = x;
-                last_y = y;
-                last_z = z;
-            }
-        }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
-
-    protected void onResume() {
-        super.onResume();
-        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    protected void onPause() {
-        super.onPause();
-        senSensorManager.unregisterListener(this);
+        startService(new Intent(this, ScreamyService.class));
     }
 }
