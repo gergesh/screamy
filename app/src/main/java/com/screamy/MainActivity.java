040994ff87408577c5f97ebc0c3ad3e482b304a5
ben_kaufman <ben.kaufman10@gmail.com>
2016-06-04 00:37:47 +0300

    Add start/stop button

--- app/src/main/java/com/screamy/MainActivity.java
+++ app/src/main/java/com/screamy/MainActivity.java
@@ -1,30 +1,63 @@
 package com.screamy;
 
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private SensorManager senSensorManager;
-    private Sensor senAccelerometer;
-
-    private long lastUpdate = 0;
-    private float last_x, last_y, last_z;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ImageButton toggleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startService(new Intent(this, ScreamyService.class));
+        toggleButton = (ImageButton) findViewById(R.id.toggle_image_button);
+        toggleButton.setOnClickListener(this);
+        if (isScreamyServiceRunning()) {
+            toggleButton.setImageResource(R.drawable.stop_button);
+        } else {
+            toggleButton.setImageResource(R.drawable.play_arrow);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.toggle_image_button:
+                if (isScreamyServiceRunning()) {
+                    stopService(new Intent(this, ScreamyService.class));
+                    toggleButton.setImageResource(R.drawable.play_arrow);
+                    
+                } else {
+                    startService(new Intent(this, ScreamyService.class));
+                    toggleButton.setImageResource(R.drawable.stop_button);
+                }
+                break;
+        }
+    }
+
+    private boolean isScreamyServiceRunning() {
+        ActivityManager manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if ("com.screamy.ScreamyService".equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
